4ddce3d4999da95a3e9d61d18d4b3109039c0394
koukiki797 <k4039340@gmail.com>
2026-03-24 08:27:30 +0900

    Update GAS_WEB_APP_URL to new endpoint

--- contact_form.js
+++ contact_form.js
@@ -1,5 +1,5 @@
 // あなたのGASのURLに書き換えてください
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyIhi4MBu0FPEBYmuO50L5eh9_RjDQ-z8kPk9rk-Jcp5wyWKMvbsGvSDugaNre6uR1j/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxFyfj61-1FnDV5dnSscy63Ynl1nhyerxvkvTOmPf6A6vX24PdEUW9-0W2f3liLptHh/exec";
 
 const form = document.getElementById("contact-form");
 const statusEl = document.getElementById("form-status");
